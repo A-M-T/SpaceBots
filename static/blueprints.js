592d340e28d7fbbaeae123658d9e4f281dc2b04e
krzys-h <krzys_h@interia.pl>
2014-02-06 17:18:00 +0100

    Increased integrity of objects

--- static/blueprints.js
+++ static/blueprints.js
@@ -97,7 +97,7 @@
 			var nf = count_features(b);
 			return nf * nf * 25 / Math.max(1, v);
 		},
-		integrity: function(b, v) { return (v+1) * 1000; }
+		integrity: function(b, v) { return (v+1) * 10000; }
 	};
 
 	var tradeoffs = {
